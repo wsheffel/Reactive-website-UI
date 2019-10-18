08cdf4842266294e02e1b05aab4e7ae37911cf3d
swatibhosle4 <56717406+swatibhosle4@users.noreply.github.com>
2019-10-18 13:52:31 +0530

    Add files via upload

---
+++ js/b2binfo.js
@@ -0,0 +1,194 @@
+function changeImage() { 
+    var url = window.location.href;
+         var args = url.split('&').pop().split('=').pop();
+             var newsrc = "../img/"+args+".jpg";
+
+                document.getElementById("productimg").src = newsrc; 
+                document.getElementById("packagedetatils").innerHTML = "Your Selection is:   1 " + args + " Subscription ";
+                
+            }
+
+
+
+$(document).ready(function() {  
+     console.log('inside ready');
+         
+             
+     $("#fname").focusout(function() {  
+                if($(this).val()=='') {  
+                    $(this).css('border', 'solid 2px red');  
+                    $("#fname").parent().after("<div id='validationfname' class='validation' style='color:red;margin-bottom: 20px;'>Please enter First Name</div>");
+
+                } 
+                else { 
+                      
+                    // If it is not blank. 
+                    $(this).css('border', 'solid 2px green');
+                    $("#validationfname").html('');
+                    $("#validationfname").removeClass("error");
+                }     
+             
+     
+        })
+     $("#fname").click(function() {  
+                $("#validationfname").html('');
+                $("#validationfname").removeClass("error");    
+      })
+     $("#fname").focusin(function() {  
+                $("#validationfname").html('');
+                $("#validationfname").removeClass("error");    
+      })
+     
+            $("#lname").focusout(function() {  
+                if($(this).val()=='') {  
+                    $(this).css('border', 'solid 2px red');
+                    $("#lname").parent().after("<div id='validationlname' class='validation' style='color:red;margin-bottom: 20px;'>Please enter Last Name</div>");
+
+                } 
+                else { 
+                      
+                    // If it is not blank. 
+                    $(this).css('border', 'solid 2px green');
+                    $("#validationlname").html('');
+                    $("#validationlname").removeClass("error");
+                }     
+             
+     
+        })
+     $("#lname").click(function() {  
+                $("#validationlname").html('');
+                $("#validationlname").removeClass("error");    
+      })
+     $("#lname").focusin(function() {  
+                $("#validationlname").html('');
+                $("#validationlname").removeClass("error");    
+      })
+     
+     $("#add1").focusout(function() {  
+                if($(this).val()=='') {  
+                    $(this).css('border', 'solid 2px red');
+                    $("#add1").parent().after("<div id='validationadd1' class='validation' style='color:red;margin-bottom: 20px;'>Please enter atleast 1 line of address</div>");
+
+                } 
+                else { 
+                      
+                    // If it is not blank. 
+                    $(this).css('border', 'solid 2px green'); 
+                    $("#validationadd1").html('');
+                    $("#validationadd1").removeClass("error");
+                }     
+             
+     
+        })
+     $("#add1").click(function() {  
+                $("#validationadd1").html('');
+                $("#validationadd1").removeClass("error");    
+      })
+     $("#add1").focusin(function() {  
+                $("#validationadd1").html('');
+                $("#validationadd1").removeClass("error");    
+      })
+     
+     $("#phonenumber").focusout(function() {  
+          var phoneno = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+
+                if(!$(this).val().match(phoneno)) {  
+                    $(this).css('border', 'solid 2px red');
+                    $("#phonenumber").parent().after("<div id='validationphn' class='validation' style='color:red;margin-bottom: 20px;'>Please enter a valid phone number</div>");
+
+                } 
+                else { 
+                      
+                    // If it is not blank. 
+                    $(this).css('border', 'solid 2px green'); 
+                    $("#validationphn").html('');
+                    $("#validationphn").removeClass("error");
+                }     
+             
+     
+        })
+     $("#phonenumber").click(function() {  
+                $("#validationphn").html('');
+                $("#validationphn").removeClass("error");    
+      })
+     $("#phonenumber").focusin(function() {  
+                $("#validationphn").html('');
+                $("#validationphn").removeClass("error");    
+      })
+                                 
+    
+     $("#zipcode").focusout(function() {  
+          var zipRegex = /[a-zA-Z][0-9][a-zA-Z](-| |)[0-9][a-zA-Z][0-9]/;
+
+                if(!$(this).val().match(zipRegex)) {  
+                    $(this).css('border', 'solid 2px red');
+                    $("#zipcode").parent().after("<div id='validationzip' class='validation' style='color:red;margin-bottom: 20px;'>Please enter a valid Zip Code</div>");
+
+                } 
+                else { 
+                      
+                    // If it is not blank. 
+                    $(this).css('border', 'solid 2px green'); 
+                    $("#validationzip").html('');
+                    $("#validationzip").removeClass("error");
+                }     
+             
+     
+        })
+     $("#zipcode").focusin(function() {  
+                $("#validationzip").html('');
+                $("#validationzip").removeClass("error");    
+      })
+     $("#zipcode").click(function() {  
+                $("#validationzip").html('');
+                $("#validationzip").removeClass("error");    
+      })
+     
+     
+    
+     
+    $("#billingButton").click(function() {  
+        
+            if($(".validation").length != 0){
+                alert("Please correct the errors and click the button again!");
+            }else{
+        
+                var fnameval = $('input[type="text"]#fname ').val();
+                var lnameval = $('input[type="text"]#lname ').val();
+                var add1val = $('input[type="text"]#add1 ').val();
+                var add2val = $('input[type="text"]#add2 ').val();
+                var add3val = $('input[type="text"]#add3 ').val();
+                var zipcodeval = $('input[type="text"]#zipcode ').val();
+                var phnval = $('input[type="text"]#phonenumber ').val();
+                var fullnameval = $('input[type="text"]#fullname ').val();
+                
+        
+        
+                $("#billingHeader").hide();
+                $("#billingDiv").hide();
+                $("#billingButton").hide();
+        
+                $("#billingveriy").show();
+                $("#billingverifyDiv").show();
+                $("#billingVerifyButton").show();
+                
+                $("#fnamevfy").text(fnameval);
+                $("#lnamevfy").text(lnameval);
+                $("#add1vfy").text(add1val);
+                $("#add2vfy").text(add2val);
+                $("#add3vfy").text(add3val);
+                $("#zipcodevfy").text(zipcodeval);
+                $("#phonenumbevfyr").text(phnval);
+
+            }
+        
+                 
+      }) 
+     
+    $("#billingVerifyButton").click(function() {  
+        
+            window.location ="../views/b2borderconfirmation.html";
+        
+                 
+      })
+        });
--- js/catalog.js
+++ js/catalog.js
@@ -1,1 +1,16 @@
-
+function openBilingPage (Str){
+    var value = Str;
+    
+    var arr = value.split("-");
+    
+    if(arr[1] == "indiv"){
+        window.location.href="views/billingPage.html?product="+arr[0];
+    }else if(arr[1] == "b2b"){
+        window.location.href="views/b2bbillingPage.html?product="+arr[0];
+     
+    }
+ 
+              
+    
+   
+}
